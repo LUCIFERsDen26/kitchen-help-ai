e928f3aca8a843328569ce31817e76d7bda7088c
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-12-03 13:19:21 +0530

    Added nutrient api  fetching endpoints

---
+++ Foodimg2Ing/routes/nutrientsRoutes.py
@@ -0,0 +1,13 @@
+# nutrientsRoutes.py
+
+from flask import Blueprint, current_app, jsonify
+from Foodimg2Ing.utils.nutrientsUtils import fetch_nutrients
+
+nutrients_api = Blueprint('nutrients_api', __name__)
+
+@nutrients_api.route('/get_nutrients/<recipename>')
+def get_nutrients(recipename):
+
+    recipenutrients = fetch_nutrients(recipename)   
+
+    return jsonify(recipenutrients)
