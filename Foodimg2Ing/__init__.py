635e4db25a91017bed3d2c0a3fd53a87350c8b0c
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-12-03 13:17:47 +0530

    Add environment variables and update Flask app to load and use them.

--- Foodimg2Ing/__init__.py
+++ Foodimg2Ing/__init__.py
@@ -1,11 +1,25 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
 
 def create_app():
     """Factory function to create and configure the Flask application."""
-    app = Flask(__name__, template_folder='Templates')
-    
-    # Import and register Blueprints
-    from Foodimg2Ing.routes import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    # Load environment variables
+    load_dotenv()
 
+    app = Flask(__name__, template_folder='Templates')
+
+    # Store the API key in app config
+    app.config['API_NINJA_API_KEY'] = os.getenv('API-NINJA_KEY')
+    app.config['NutritionixAPI_applicationID'] = os.getenv('NutritionixAPI_applicationID')
+    app.config['NutritionixAPI_applicationKEY'] = os.getenv('NutritionixAPI_applicationKEY')
+
+    # Import and register Blueprints
+    #from Foodimg2Ing.routes.routesMain import main as main_blueprint
+    from Foodimg2Ing.routes.routesApiNinija import recipeApi as recipeApi_blueprint
+    from Foodimg2Ing.routes.nutrientsRoutes import nutrients_api 
+    #app.register_blueprint(main_blueprint)
+    app.register_blueprint(recipeApi_blueprint)
+    app.register_blueprint(nutrients_api)
     return app
+
